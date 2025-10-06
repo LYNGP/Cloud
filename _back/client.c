d6da6035221ea406773b41b9ec775f65aad328e3
QWEADDGFG <231060023@hdu.edu.cn>
2025-10-06 16:18:55 +0800

    update 20251006 16:18:55

--- _back/client.c
+++ _back/client.c
@@ -12,8 +12,8 @@ int main()
     serveraddr.sin_port = htons(8080);
     serveraddr.sin_addr.s_addr = inet_addr("10.14.118.48");
 
-    int ret = connect(clientfd, 
-                      (const struct sockaddr *)&serveraddr, 
+    int ret = connect(clientfd,
+                      (const struct sockaddr *)&serveraddr,
                       sizeof(serveraddr));
     ERROR_CHECK(ret, -1, "connect");
 
@@ -24,7 +24,7 @@ int main()
     printf("ret: %d, filename's len: %d\n", ret, len);
     ret = recv(clientfd, filename, len, MSG_WAITALL);
     printf("ret: %d, recv msg: %s\n", ret, filename);
-    
+
     // 修改文件保存路径到bin目录
     char filepath[200] = {0};
     snprintf(filepath, sizeof(filepath), "bin/%s", filename);
@@ -36,20 +36,22 @@ int main()
     recv(clientfd, &len, sizeof(len), MSG_WAITALL);
     recv(clientfd, &file_length, len, MSG_WAITALL);
     printf("file length: %ld\n", file_length);
-    
+
     // 接收文件内容（服务端用sendfile直接发送）
     char buff[4096] = {0};
     off_t received = 0;
-    while(received < file_length) {
+    while (received < file_length)
+    {
         int to_recv = (file_length - received > sizeof(buff)) ? sizeof(buff) : (file_length - received);
         ret = recv(clientfd, buff, to_recv, 0);
-        if(ret <= 0) {
+        if (ret <= 0)
+        {
             break;
         }
         write(wfd, buff, ret);
         received += ret;
     }
-    
+
     printf("File received successfully: %ld bytes\n", received);
     close(wfd);
     close(clientfd);
